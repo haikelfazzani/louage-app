4c067ee4d4a7d8705989aee010d14866a1c9258c
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-09-06 19:03:45 +0100

    update app

--- dao/utilisateurs.dao.js
+++ dao/utilisateurs.dao.js
@@ -1,11 +1,26 @@
 const db = require('../database/connection');
+var SqlString = require('sqlstring');
 
 const table = {
+  name: 'utilisateurs',
   id: 'id', email: 'email', password: 'password', avatar: 'avatar', role: 'role'
 }
 
-function addUser () {
+const UtilisateurDao = {
+  addUser (email, password, avatar, role) {
+    const rq = `INSERT INTO 
+    ${table.name} (${table.email}, ${table.password}, ${table.avatar}, ${table.role})
+    values(?, ? , ? , ?)`;
 
+    const sql = SqlString.format(rq, [email, password, avatar, role]);
+
+    return new Promise((resolve, reject) => {
+      db.query(sql, (err, result) => {
+        if (err) reject(err)
+        else resolve(result)
+      })
+    })
+  }
 }
 
-module.exports = addUser;
+module.exports = UtilisateurDao;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const UtilisateurDao = require('../dao/utilisateurs.dao');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  
+  UtilisateurDao.addUser('admin@gmail.com', '123', '', 'admin')
+  .then(result => console.log(result))
+  .catch(err => console.log(err))
+
   res.send('respond with a resource');
 });
 
