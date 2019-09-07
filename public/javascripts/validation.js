0e4da13609e9b407004cc2b9b1438bd7328eabbf
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-09-07 23:03:20 +0100

    form login, register validation client

---
+++ public/javascripts/validation.js
@@ -0,0 +1,23 @@
+// validation pour les formulaire : login et register
+
+let formRegister = document.querySelector('.form-signin');
+//email ou mot de passe incorrect!
+function isPureStr (str) {
+  return /^[a-z0-9@\.\-]*$/gi.test(str)
+}
+
+formRegister.onsubmit = e => {
+
+  let email = e.target.email.value;
+  let pass = e.target.password.value;  
+
+  if(isPureStr(email) && isPureStr(pass)) {
+    return true
+  }
+  else {
+    let msgForm = document.querySelector('#form-signin');
+    msgForm.style.display = 'block';
+    msgForm.textContent = 'Email ou mot de passe contient des charactéres spéciaux';
+    return false;
+  }
+}
