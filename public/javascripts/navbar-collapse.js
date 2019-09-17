0291e2c4b8ec58c9b1cb173c35d37d23e552a5f3
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-09-17 22:09:13 +0100

    update ui

--- public/javascripts/navbar-collapse.js
+++ public/javascripts/navbar-collapse.js
@@ -1,14 +1,8 @@
-let btnCollapse = document.querySelector('.navbar-toggler');
-
-btnCollapse.onclick = () => {
-  let dropDown = document.getElementById('navbarNav');
-  dropDown.style.display = dropDown.style.display === 'none' ? 'block' : 'none';
-}
-
-
 let profileIcon = document.getElementById('navbarDropdown')
 
-profileIcon.onclick = () => {
-  let dropDown = document.querySelector('.dropdown-menu')
-  dropDown.style.display = dropDown.style.display === 'none' ? 'block' : 'none';
+if(profileIcon) {
+  profileIcon.onclick = () => {
+    let dropDown = document.querySelector('.dropdown-menu')
+    dropDown.style.display = dropDown.style.display === 'none' ? 'block' : 'none';
+  }
 }
