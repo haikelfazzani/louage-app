90a9db90c5ed474c73552076bddbe996c3c04cb0
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-09-18 21:13:24 +0100

    update routes, ui vehicule

--- routes/chefstation/vehicules.js
+++ routes/chefstation/vehicules.js
@@ -1,6 +1,9 @@
 var router = require('express').Router()
-var vehiculeDao = require('../../dao/vehicules.dao')
 var { checkUserConnected } = require('../../middleware/authorisation')
+
+var vehiculeDao = require('../../dao/vehicules.dao')
+var stationDao = require('../../dao/stations.dao')
+var Vehicule = require('../../model/Vehicule.model')
 
 router.get('/', checkUserConnected, (req, res) => {
   vehiculeDao.getVehicules()
@@ -14,7 +17,27 @@ router.get('/', checkUserConnected, (req, res) => {
 
 
 router.get('/ajout', checkUserConnected, (req, res) => {
-  res.render('admin/vehicule/ajout')
+  stationDao.getStations()
+    .then(stations => {
+      res.render('admin/vehicule/ajout', { stations })
+    })
+    .catch(error => {
+      res.render('admin/vehicule/ajout')
+    })
+})
+
+router.post('/ajout', checkUserConnected, (req, res) => {
+
+  let { proprietaire, numSerie, nbPlaces, tel, station } = req.body
+  let newVehicule = new Vehicule(proprietaire, numSerie, nbPlaces, tel, station)
+
+  vehiculeDao.addVehicule(newVehicule)
+    .then(result => {
+      res.render('admin/vehicule/ajout', { msg: 'une nouvelle véhicule à été bien ajoutée' })
+    })
+    .catch(error => {
+      res.render('admin/vehicule/ajout', { msg: 'erreur d\'ajout' })
+    })
 })
 
 module.exports = router
