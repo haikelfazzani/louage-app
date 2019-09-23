3237e017ef4309131231ee289d8180b00244462a
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-09-24 00:34:07 +0100

    paginationn

--- routes/client/reservations.js
+++ routes/client/reservations.js
@@ -21,7 +21,7 @@ function checkValidParam (req, res, next) {
 
 router.get('/', checkUserConnected, (req, res) => {
 
-  let allVoyages =JSON.parse(Buffer.from(req.query.v, 'base64').toString())
+  let allVoyages = JSON.parse(Buffer.from(req.query.v, 'base64').toString())
   let { destination, nom_station, timestamp_voyage } = allVoyages
 
   Promise.all([
@@ -45,10 +45,16 @@ router.get('/', checkUserConnected, (req, res) => {
 router.get('/all', checkUserConnected, (req, res) => {
 
   let { email } = req.session.userInfo
+  let { b, e } = req.query
 
   reservDao.getReservByUser(email)
     .then(reservations => {
-      reservations.sort((i,j) => i.heure_depart >= j.heure_depart && Date.parse(i.date_depart) >= Date.parse(j.date_depart))
+
+      reservations = isNaN(b) || isNaN(e)
+        ? reservations = reservations.slice(0, 5) : b < 0 && e < 5
+        ? reservations = reservations.slice(0, 5) : reservations.slice(b || 0, e || 5)
+
+      reservations.sort((i, j) => i.heure_depart >= j.heure_depart && Date.parse(i.date_depart) >= Date.parse(j.date_depart))
       res.render('client/profile/reservations', { reservations })
     })
     .catch(error => {
@@ -72,12 +78,12 @@ router.get('/annuler', checkUserConnected, function (req, res) {
     reservDao.updateEtatReserv('annuler', r),
     paymentDao.cancelPayment(r)
   ])
-  .then(values => {
-    res.redirect('/reservations/all')  
-  })
-  .catch(error => {
-    res.redirect('/404')  
-  })  
+    .then(values => {
+      res.redirect('/reservations/all')
+    })
+    .catch(error => {
+      res.redirect('/404')
+    })
 });
 
 module.exports = router
