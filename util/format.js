12af00dccfd722854771cec0354186ad3f87f221
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-10-02 12:40:20 +0100

    update app

--- util/format.js
+++ util/format.js
@@ -1,14 +1,8 @@
 module.exports = (function (date) { // 2019-08-16
   const months = [
     "jan", "fév", "mars", "avril", "mai", "juin", "juil", "août", "sep", "oct", "nov", "déc"
-  ];
-
-  const jours = ['samedi', 'dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi']
-
+  ]
+  const jours = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi']
   let d = new Date(date)
-  let j = d.getDay()
-  let m = d.getMonth()
-  let y = d.getFullYear()
-  
-  return jours[j] + ', ' + (d.getDate() - 1) + ' ' + months[m] + ' ' + y
+  return jours[d.getDay()] + ', ' + d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear()
 })
