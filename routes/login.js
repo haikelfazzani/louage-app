945f51cf6531980ee28d05aeb8d15cf589cc3501
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-10-02 14:52:41 +0100

    update app

--- routes/login.js
+++ routes/login.js
@@ -1,34 +1,25 @@
 var router = require('express').Router()
 var { isConnected } = require('../middleware/authorisation')
-var UtilisateurDao = require('../dao/utilisateurs.dao');
-const bcrypt = require('bcrypt')
-const checkValidEmail = require('../middleware/checkValidEmail')
+var bcrypt = require('bcrypt')
+var checkValidEmail = require('../middleware/checkValidEmail')
+var checkUserExist = require('../middleware/checkUserExist')
 
 router.get('/', isConnected, function (req, res) {
   res.render('login')
-});
+})
 
-router.post('/', [isConnected, checkValidEmail], function (req, res) {
-  let { email, password } = req.body
-
-  UtilisateurDao.getUser(email)
-    .then(result => {
-      let connectedUser = result[0]
-      bcrypt.compare(password, connectedUser.password)
-        .then(async (hashRes) => {
-
-          if (hashRes && email === connectedUser.email) {
-            req.session.userInfo = connectedUser
-            res.redirect('/')
-          }
-          else {
-            res.render('login', { msg: 'vous devez s\'inscire avant de se connecter!' })
-          }
-        })
-        .catch(errHash => { res.render('error', { appErrors: errHash }) });
+router.post('/', [isConnected, checkValidEmail, checkUserExist], function (req, res) {
+  let { email, password, connectedUser } = req.body
+  bcrypt.compare(password, connectedUser.password)
+    .then(async (hashRes) => {
+      if (hashRes && email === connectedUser.email) {
+        req.session.userInfo = connectedUser
+        res.redirect('/')
+      }
+      else {
+        res.render('login', { msg: 'vous devez s\'inscire avant de se connecter!' })
+      }
     })
-    .catch(error => {
-      res.render('login', { msg: 'ce compte n\'existe pas!' })
-    })
+    .catch(e => { res.redirect('/404') })
 })
 module.exports = router
